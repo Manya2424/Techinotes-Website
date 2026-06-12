7f83afbf0fafb4a2ff68257198a0f2656ab82758
Manya2424 <manyasingla13@gmail.com>
2026-06-12 13:43:11 +0530

    Add automatic superuser creation

---
+++ create_superuser.py
@@ -0,0 +1,25 @@
+import os
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project_files.settings")
+
+import django
+django.setup()
+
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
+email = os.environ.get("DJANGO_SUPERUSER_EMAIL")
+password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
+
+if username and email and password:
+    if not User.objects.filter(username=username).exists():
+        User.objects.create_superuser(
+            username=username,
+            email=email,
+            password=password
+        )
+        print("Superuser created")
+    else:
+        print("Superuser already exists")
